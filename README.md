6225cf3fc9c542770408ef155195126bfe3e9a69
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-08-17 01:29:14 -0500

    got initial pw gen to work without validation

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A password generator that will create a pw using password criteria you select.
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 https://www.w3schools.com/jsref/jsref_charat.asp
+
+https://www.w3schools.com/js/js_strings.asp
--- script.js
+++ script.js
@@ -6,8 +6,80 @@ function generatePassword() {
     // Creating strings to create the password  
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var symbols = " !#$%&'\"()*+,-./:;<=>?@[\\]^_`{|}~";
     var numbers = "0123456789";
+    var charPool {
+        lower: "abcdefghijklmnopqrstuvwxyz",
+        upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        sym: " !#$%&'\"()*+,-./:;<=>?@[\\]^_`{|}~",
+        num: "0123456789"
+    }
+    var length = 0;
+    var characters = "";
+    var pwGenOut = "";
+
+
+    // prompts asking for each character user wants
+    var userInLower = window.confirm("Do you want lowercase characters?");
+    if (userInLower) {
+        characters += lowercase;
+    };
+    var userInUpper = window.confirm("Do you want uppercase characters?");
+    if (userInUpper) {
+        characters += uppercase;
+    };
+    var userInSymbols = window.confirm("Do you want symbol characters?");
+    if (userInSymbols) {
+        characters += symbols;
+    };
+    var userInNum = window.confirm("Do you want number characters?");
+    if (userInNum) {
+        characters += numbers;
+    };
+
+
+    //while loop to check to see if there are no selections
+    // while (!userInLower && !userInUpper && !userInSymbols && userInNum) {
+    //     alert("Please select one of the password criteria");
+    //     userInLower = window.confirm("Do you want lowercase characters?");
+    //     userInUpper = window.confirm("Do you want uppercase characters?");
+    //     userInSymbols = window.confirm("Do you want symbol characters?");
+    //     userInNum = window.confirm("Do you want uppercase characters?");
+    // }
+    // adding selection criteria to characters pool 
+
+
+    // prompt asking how long the password length will be
+    var userInLength = window.prompt("How many characters would you like?");
+    // removing typeof(userInLength) !== 'number' || to check to see if that was causing while loop to run forever
+    // while (userInLength < 8 || userInLength > 128) {
+    //     window.alert("Password is minimum length of 8 and maximum of 128 characters. Please input how many characters");
+    // }
+    // //-----------------------------------------------------------------------------------------------------------------------------------
+    //maybe i don't need the if else if i just use a while loop
+    //if (userInLength < 8) { // checking to see if minimum character is met 
+    //    window.alert("Minimum of 8 characters, please enter desired character length again");
+    //    userInLength = window.prompt("How many characters would you like?");
+    //} else if (userInLength > 128) { // checking to see if maximum charcter isn't exceeded
+    //    window.alert("Maximum of 128 characters, please enter desired character length again");
+    //    userInLength = window.prompt("How many characters would you like?");
+    //} else if (typeof(userInLength) !== 'number') { // checking to see user entered any non numbers 
+    //    window.alert("Please enter in a number!");
+    //    userInLength = window.prompt("How many characters would you like?");}
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // assigning length to the user input 
+    length = userInLength;
+
+    // For loop to create the password based on the selection criteria
+    for (var i = 0; i < length; i++) {
+        //generates random number to select character from character pool
+        pwGenOut += characters[Math.floor(Math.random() * characters.length)]
+    }
+    return pwGenOut;
+
+    password = pwGenOut;
+    //alert("Password: " + password);
+
 }
 // Write password to the #password input
 function writePassword() {
